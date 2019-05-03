01de77fe989b9777f4277474643ecff932d574cd
blubrandi <blubrandi0@gmail.com>
2019-05-03 08:28:34 -0600

    Configured knex file

--- knexfile.js
+++ knexfile.js
@@ -3,8 +3,21 @@
 module.exports = {
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
-      filename: './dev.sqlite3'
+      filename: './data/dishdb.db3' // the folder will be created when we run the migrations
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+    // Enforces SQLite Foreign Keys, make sure to add!!
+    pool: {
+      afterCreate: (connection, done) => {
+        connection.run('PRAGMA foreign_keys =ON', done);
+      }
     }
   }
 };
