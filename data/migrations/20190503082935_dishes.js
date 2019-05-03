cd1c34ea0ff5bd168155ede65c0a66f6c6d4bb3b
blubrandi <blubrandi0@gmail.com>
2019-05-03 08:42:33 -0600

    Added tables schema to migration.

--- data/migrations/20190503082935_dishes.js
+++ data/migrations/20190503082935_dishes.js
@@ -1,8 +1,55 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema
+    .createTable('dishes', tbl => {
+      tbl.increments();
+
+      tbl
+        .string('name', 128)
+        .notNullable()
+        .unique();
+    })
+    .createTable('recipes', tbl => {
+      tbl.increments();
+
+      tbl
+        .string('name', 128)
+        .notNullable()
+        .unique();
+      tbl
+        .integer('dishes_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('dishes')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE');
+    })
+    .createTable('ingredients', tbl => {
+      tbl.increment();
+
+      tbl
+        .integer('recipes_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('cohorts')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE');
+
+      tbl
+        .integer('ingredients_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('ingredients')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE');
+    });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema
+    .dropTableIfExists('dishes')
+    .dropTableIfExists('recipes')
+    .dropTableIfExists('ingredients');
 };
